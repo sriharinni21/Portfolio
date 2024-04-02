077452e2cb1d0b2ca873f967117718b57e85e42e
sriharinni21 <sriharinni.je2022it@sece.ac.in>
2024-04-02 14:07:52 +0530

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu) {
+        navMenu.classList.remove('show-menu');
+    }
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    if (header) {
+        this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+    }
+};
+
+window.addEventListener('scroll', shadowHeader);
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_maas4ws', 'template_st9pw9j', '#contact-form', 'oOK18LuKWOWd4xwif')
+        .then(() => {
+            contactMessage.textContent = 'Message sent successfully ✅';
+
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000);
+
+            contactForm.reset();
+
+        }, () => {
+            contactMessage.textContent = 'Message not sent (service error) ❌';
+        });
+};
+
+contactForm.addEventListener('submit', sendEmail);
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (sectionClass) {
+            if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                sectionClass.classList.add('active-link');
+            } else {
+                sectionClass.classList.remove('active-link');
+            }
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1000,
+    delay: 100,
+})
+
+sr.reveal('.home__profile, .about__image, .contact__mail', { origin: 'right' });
+sr.reveal('.home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data', { origin: 'left' });
+sr.reveal('.services__card, .projects__card', { interval: 100 });
+
+
